e4efe18288e796c8b59d7dc92038f89a6ac7f9d1
группы стационара <st@itstep.lan>
2020-11-23 21:55:12 +0300

    step

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+const gulp = require ('gulp');
+const sass = require ('gulp-sass');
+const cssnano = require('gulp-cssnano');
+const rename = require('gulp-rename');
+const browserSync = require('browser-sync').create();
+
+function style() {
+    return gulp.src('src/scss/main.scss')
+        .pipe(sass())
+        .pipe(cssnano())
+        .pipe(rename('style.css'))
+        .pipe(gulp.dest('./'))
+        .pipe(browserSync.stream());
+}
+
+function watch() {
+    browserSync.init({
+        server: {
+           baseDir: "./",
+           index: "./index.html"
+        }
+    });
+    gulp.watch('src/scss/**/*.scss', style)
+}
+
+exports.watch = watch;
+
+
