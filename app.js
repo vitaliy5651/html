34012672b85d415868fd5745898bbf7bc98ac3b4
группы стационара <st@itstep.lan>
2021-01-18 21:58:02 +0300

    22:00 18.01.2021

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
+const { response } = require('express');
 const express = require('express');
 const app = express();
-const { Client } = require('pg')
+const { Client } = require('pg');
 
 const client = new Client({
-    connectionString: 'postgres://hdfevoewigwnim:4ab6df38fe15be40a8488f7a1619605f55dae7241e9d35d5e24b092aa042ce2a@ec2-34-252-98-12.eu-west-1.compute.amazonaws.com:5432/d4la33od7ugl70',
-    ssl: { rejectUnauthorized: false }
+	connectionString: 'postgres://hdfevoewigwnim:4ab6df38fe15be40a8488f7a1619605f55dae7241e9d35d5e24b092aa042ce2a@ec2-34-252-98-12.eu-west-1.compute.amazonaws.com:5432/d4la33od7ugl70',
+	ssl: { rejectUnauthorized: false }
 });
 
 client.connect(err => {
@@ -12,26 +13,49 @@ client.connect(err => {
       console.error('connection error', err.stack)
     } else {
       console.log('connected');
-      client.query(`INSERT INTO Persons (lastname, firstname)
-      VALUES ('john', 'Doe');`,
-    (err,res) => {
-        if(err){
-            console.log(err)
-        } else {
-            console.log(res);
-        }
+    //   client.query(`SELECT * FROM Persons`,
+    //                 (err, res) => {
+    //                     if (err) {
+    //                          console.log(err)
+    //                     } else {
+    //                         console.log(res.rows);
+    //                     }
 
-        } 
-    )
+    //                 }
+    //     )
     }
-  })
+})
+
+app.set('view engine', 'pug');
 
 app.use(express.static('dist'));
 
 app.get('/', function(req, res){
     res.send('Hello');
 });
+app.get('/main', function(req, res) {
+    res.sendFile(__dirname + "/index.html");
+})
+
+
+app.get('/userlist', function(req,res){
+
+     client.query('SELECT * FROM Persons', function(err,response){
+         if(err) {
+             res.send('Error');
+         } else{
+            res.render('index', { title: 'Hey', users: response.rows});
+         }
+     })
+
+    
+})
+
+app.get('/adduser', function(req,res){
+    res.render('addUser');
+})
+
 
 app.listen(3000, function() {
-    console.log('sucess');
+    console.log('success');
 });
