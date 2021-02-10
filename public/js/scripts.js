7da4f16c43c0693bce6d08d796d95170367348e5
Vitaliy <vitaliy.kaliskarov@mail.ru>
2021-02-10 17:42:07 +0300

    17:41 10.02.2021

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -63,3 +63,27 @@ document.addEventListener('DOMContentLoaded', function() {
    })
 
 });
+
+let listofNumbers = [1,2,3,4,5];
+let a = listofNumbers.reverse();
+console.log(a);
+
+
+function a(nums,k){
+for(let i =0; i<nums.length-1;i++){
+    for(let j =i+1; j<nums.length;j++){
+        if(nums[i]+nums[j]===k){
+            console.log(true);
+        }else{
+            console.log(false);
+        }
+    }
+}
+}
+
+var nums1 =[46,55,88,0,0,0,0];
+var nums2 =[18,29,80,90];  
+var nums3 = nums1.concat(nums2);
+nums3.sort();
+nums3.splice(0,4);
+console.log(nums3);
