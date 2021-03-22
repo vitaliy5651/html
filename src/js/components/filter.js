773b6be2812d547f86a3c66b4173e47c5f687919
группы стационара <st@itstep.lan>
2021-03-22 19:29:04 +0300

    19:31 22.03.2021

---
+++ src/js/components/filter.js
@@ -0,0 +1,9 @@
+// document.addEventListener('DOMContentloaded',() =>{
+//     const buttons = document.querySelector('.btn-group');
+//     const products = document.querySelector('.products');
+
+//     buttons.addEventListener('click', (e) =>{
+//         if(e.target.nodeName = 'BUTTON' && e.target.dataset && e.target)
+//     })
+    
+// });
